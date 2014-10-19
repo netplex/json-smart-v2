da00dba8176efb99257cb249fbbb3bd0c2391828
uriel <uchemouni@gmail.com>
2014-10-19 21:43:35 +0200

    disable JDK 8 junit fot standard deployment.

--- json-smart/src/test/java/net/minidev/json/testMapping/TestCustomMappingInstant.java
+++ json-smart/src/test/java/net/minidev/json/testMapping/TestCustomMappingInstant.java
@@ -7,7 +7,6 @@ import junit.framework.TestCase;
 import net.minidev.json.JSONStyle;
 import net.minidev.json.JSONValue;
 import net.minidev.json.parser.ParseException;
-import net.minidev.json.writer.JsonReader;
 
 /**
  * Test JDK 8+ java.time.Instant
@@ -28,46 +27,46 @@ public class TestCustomMappingInstant extends TestCase {
 	}
 	
 // Need JDK 1.8
-	public void test_instant() {
-		JSONValue.registerWriter(java.time.Instant.class, new net.minidev.json.reader.JsonWriterI<java.time.Instant>() {
-			@Override
-			public void writeJSONString(java.time.Instant value, Appendable out, JSONStyle compression)
-					throws IOException {
-				if (value == null)
-					out.append("null");
-				else
-					out.append(Long.toString(value.toEpochMilli()));
-			}
-		});
- 
-		JSONValue.registerReader(RegularClass.class, new net.minidev.json.writer.JsonReaderI<RegularClass>(JSONValue.defaultReader) {
-			@Override
-			public void setValue(Object current, String key, Object value) throws ParseException, IOException {
-				if (key.equals("instant")) {
-					java.time.Instant inst = java.time.Instant.ofEpochMilli((((Number)value).longValue()));
-					((RegularClass)current).setInstant(inst);
-				}
-			}
-			@Override
-			public Object createObject() {
-				return new RegularClass();
-			}
-		});
-		java.time.Instant instant = java.time.Instant.now();
-		RegularClass regularClass = new RegularClass();
-		regularClass.setInstant(instant);
-		String data = JSONValue.toJSONString(regularClass);
-		RegularClass result = JSONValue.parse(data, RegularClass.class);
-		Assert.assertEquals(result.getInstant(), instant);
-	}
-
-	public static class RegularClass {
-		private java.time.Instant instant;
-		public java.time.Instant getInstant() {
-			return instant;
-		}
-		public void setInstant(java.time.Instant instant) {
-			this.instant = instant;
-		}
-	}
+//	public void test_instant() {
+//		JSONValue.registerWriter(java.time.Instant.class, new net.minidev.json.reader.JsonWriterI<java.time.Instant>() {
+//			@Override
+//			public void writeJSONString(java.time.Instant value, Appendable out, JSONStyle compression)
+//					throws IOException {
+//				if (value == null)
+//					out.append("null");
+//				else
+//					out.append(Long.toString(value.toEpochMilli()));
+//			}
+//		});
+// 
+//		JSONValue.registerReader(RegularClass.class, new net.minidev.json.writer.JsonReaderI<RegularClass>(JSONValue.defaultReader) {
+//			@Override
+//			public void setValue(Object current, String key, Object value) throws ParseException, IOException {
+//				if (key.equals("instant")) {
+//					java.time.Instant inst = java.time.Instant.ofEpochMilli((((Number)value).longValue()));
+//					((RegularClass)current).setInstant(inst);
+//				}
+//			}
+//			@Override
+//			public Object createObject() {
+//				return new RegularClass();
+//			}
+//		});
+//		java.time.Instant instant = java.time.Instant.now();
+//		RegularClass regularClass = new RegularClass();
+//		regularClass.setInstant(instant);
+//		String data = JSONValue.toJSONString(regularClass);
+//		RegularClass result = JSONValue.parse(data, RegularClass.class);
+//		Assert.assertEquals(result.getInstant(), instant);
+//	}
+//
+//	public static class RegularClass {
+//		private java.time.Instant instant;
+//		public java.time.Instant getInstant() {
+//			return instant;
+//		}
+//		public void setInstant(java.time.Instant instant) {
+//			this.instant = instant;
+//		}
+//	}
 }
