47a5b023896d2d9c6139d52e6800e2ac253a2332
dpeger <dpeger@users.noreply.github.com>
2021-04-23 22:01:20 +0200

    Test improvments
    Modified test to use Junit4's `Parameterized` runner and added an emoji test

--- json-smart/src/test/java/net/minidev/json/test/TestUtf8.java
+++ json-smart/src/test/java/net/minidev/json/test/TestUtf8.java
@@ -1,83 +1,79 @@
 package net.minidev.json.test;
 
-import java.io.ByteArrayInputStream;
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-
 import junit.framework.TestCase;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class TestUtf8 extends TestCase {
 
-	public static Map<String, String> nonLatinTexts = new HashMap<String, String>();
-	static {
-		nonLatinTexts.put("Sinhala", "සිංහල ජාතිය");
-		nonLatinTexts.put("Japanese", "日本語");
-		nonLatinTexts.put("Russian", "Русский");
-		nonLatinTexts.put("Farsi", "فارسی");
-		nonLatinTexts.put("Korean", "한국어");
-		nonLatinTexts.put("Armenian", "Հայերեն");
-		nonLatinTexts.put("Hindi", "हिन्दी");
-		nonLatinTexts.put("Hebrew", "עברית");
-		nonLatinTexts.put("Chinese", "中文");
-		nonLatinTexts.put("Amharic", "አማርኛ");
-		nonLatinTexts.put("Malayalam", "മലയാളം");
-		nonLatinTexts.put("Assyrian Neo-Aramaic", "ܐܬܘܪܝܐ");
-		nonLatinTexts.put("Georgian", "მარგალური");
+    @Parameterized.Parameter(0)
+    public String language;
 
-	}
+    @Parameterized.Parameter(1)
+    public String nonLatinText;
 
-	public void testString() throws Exception {
-		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
-			String language = nonLatinEntry.getKey();
-			String nonLatinText = nonLatinEntry.getValue();
+    @Parameterized.Parameters(name = "{index}: language=''{0}'', text=''{1}''")
+    public static Collection<Object[]> nonLatinTexts() {
+        List<Object[]> nonLatinTexts = new ArrayList<Object[]>();
+        nonLatinTexts.add(new Object[]{"Sinhala", "සිංහල ජාතිය"});
+        nonLatinTexts.add(new Object[]{"Japanese", "日本語"});
+        nonLatinTexts.add(new Object[]{"Russian", "Русский"});
+        nonLatinTexts.add(new Object[]{"Farsi", "فارسی"});
+        nonLatinTexts.add(new Object[]{"Korean", "한국어"});
+        nonLatinTexts.add(new Object[]{"Armenian", "Հայերեն"});
+        nonLatinTexts.add(new Object[]{"Hindi", "हिन्दी"});
+        nonLatinTexts.add(new Object[]{"Hebrew", "עברית"});
+        nonLatinTexts.add(new Object[]{"Chinese", "中文"});
+        nonLatinTexts.add(new Object[]{"Amharic", "አማርኛ"});
+        nonLatinTexts.add(new Object[]{"Malayalam", "മലയാളം"});
+        nonLatinTexts.add(new Object[]{"Assyrian Neo-Aramaic", "ܐܬܘܪܝܐ"});
+        nonLatinTexts.add(new Object[]{"Georgian", "მარგალური"});
+        nonLatinTexts.add(new Object[]{"Emojis", "🐶🐱🐭🐹🐰🦊🐻🐼🐻‍❄🐨🐯🦁🐮🐷🐽🐸🐵🙈🙉🙊🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝🐛"});
+        return nonLatinTexts;
+    }
 
-			String s = "{\"key\":\"" + nonLatinText + "\"}";
-			JSONObject obj = (JSONObject) JSONValue.parse(s);
-			String actual = (String) obj.get("key");
-			assertEquals("Parsing String " + language + " text", nonLatinText, actual);
-		}
-	}
+    @Test
+    public void testString() {
+        String s = "{\"key\":\"" + nonLatinText + "\"}";
+        JSONObject obj = (JSONObject) JSONValue.parse(s);
+        String actual = (String) obj.get("key");
+        assertEquals("Parsing String " + language + " text", nonLatinText, actual);
+    }
 
-	public void testReader() throws Exception {
-		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
-			String language = nonLatinEntry.getKey();
-			String nonLatinText = nonLatinEntry.getValue();
+    @Test
+    public void testReader() {
+        String s = "{\"key\":\"" + nonLatinText + "\"}";
+        StringReader reader = new StringReader(s);
+        JSONObject obj = (JSONObject) JSONValue.parse(reader);
+        String actual = (String) obj.get("key");
+        assertEquals("Parsing StringReader " + language + " text", nonLatinText, actual);
+    }
 
-			String s = "{\"key\":\"" + nonLatinText + "\"}";
-			StringReader reader = new StringReader(s);
-			JSONObject obj = (JSONObject) JSONValue.parse(reader);
-			String actual = (String) obj.get("key");
-			assertEquals("Parsing StringReader " + language + " text", nonLatinText, actual);
-		}
-	}
+    @Test
+    public void testInputStream() throws Exception {
+        String s = "{\"key\":\"" + nonLatinText + "\"}";
+        ByteArrayInputStream bis = new ByteArrayInputStream(s.getBytes("utf8"));
+        JSONObject obj = (JSONObject) JSONValue.parse(bis);
+        String actual = (String) obj.get("key");
+        assertEquals("Parsing ByteArrayInputStream " + language + " text", nonLatinText, actual);
+    }
 
-	public void testInputStream() throws Exception {
-		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
-			String language = nonLatinEntry.getKey();
-			String nonLatinText = nonLatinEntry.getValue();
-
-			String s = "{\"key\":\"" + nonLatinText + "\"}";
-			ByteArrayInputStream bis = new ByteArrayInputStream(s.getBytes("utf8"));
-			JSONObject obj = (JSONObject) JSONValue.parse(bis);
-			String actual = (String) obj.get("key");
-			assertEquals("Parsing ByteArrayInputStream " + language + " text", nonLatinText, actual);
-		}
-	}
-
-	public void testBytes() throws Exception {
-		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
-			String language = nonLatinEntry.getKey();
-			String nonLatinText = nonLatinEntry.getValue();
-
-			String s = "{\"key\":\"" + nonLatinText + "\"}";
-			byte[] bs = s.getBytes("utf8");
-			JSONObject obj = (JSONObject) JSONValue.parse(bs);
-			String actual = (String) obj.get("key");
-			assertEquals("Parsing bytes[] " + language + " text", nonLatinText, actual);
-		}
-	}
+    @Test
+    public void testBytes() throws Exception {
+        String s = "{\"key\":\"" + nonLatinText + "\"}";
+        byte[] bs = s.getBytes("utf8");
+        JSONObject obj = (JSONObject) JSONValue.parse(bs);
+        String actual = (String) obj.get("key");
+        assertEquals("Parsing bytes[] " + language + " text", nonLatinText, actual);
+    }
 }
