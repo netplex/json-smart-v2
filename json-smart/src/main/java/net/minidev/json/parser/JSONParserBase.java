b3d52aa168e5a474c70a8e9ec69f823c2e7216ca
dpeger <dpeger@users.noreply.github.com>
2021-04-23 17:38:38 +0200

    [#60][#62] Unchecked Exception in Parser
    Caught unchecked `NumberFormatException` during float parsing and converted into checked `ParseException`.
    
    Fixes #60

--- json-smart/src/main/java/net/minidev/json/parser/JSONParserBase.java
+++ json-smart/src/main/java/net/minidev/json/parser/JSONParserBase.java
@@ -139,11 +139,15 @@ abstract class JSONParserBase {
 	protected Number extractFloat() throws ParseException {
 		if (!acceptLeadinZero)
 			checkLeadinZero();
-		if (!useHiPrecisionFloat)
-			return Float.parseFloat(xs);
-		if (xs.length() > 18) // follow JSonIJ parsing method
-			return new BigDecimal(xs);
-		return Double.parseDouble(xs);
+		try {
+			if (!useHiPrecisionFloat)
+				return Float.parseFloat(xs);
+			if (xs.length() > 18) // follow JSonIJ parsing method
+				return new BigDecimal(xs);
+			return Double.parseDouble(xs);
+		} catch (NumberFormatException e) {
+			throw new ParseException(pos, ERROR_UNEXPECTED_TOKEN, xs);
+		}
 	}
 
 	/**
