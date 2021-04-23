42e4e68f23ffd1c6a1f037a41c9c00f96695ca03
dpeger <dpeger@users.noreply.github.com>
2021-04-23 18:41:15 +0200

    Test improvments

--- json-smart/src/test/java/net/minidev/json/test/TestUtf8.java
+++ json-smart/src/test/java/net/minidev/json/test/TestUtf8.java
@@ -2,53 +2,82 @@ package net.minidev.json.test;
 
 import java.io.ByteArrayInputStream;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 import junit.framework.TestCase;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
 
 public class TestUtf8 extends TestCase {
-	// Sinhalese language
-	static String[] nonLatinTexts = new String[] { "සිංහල ජාතිය", "日本語", "Русский", "فارسی", "한국어", "Հայերեն", "हिन्दी", "עברית", "中文", "አማርኛ", "മലയാളം",
-			"ܐܬܘܪܝܐ", "მარგალური" };
+
+	public static Map<String, String> nonLatinTexts = new HashMap<String, String>();
+	{
+		nonLatinTexts.put("Sinhala", "සිංහල ජාතිය");
+		nonLatinTexts.put("Japanese", "日本語");
+		nonLatinTexts.put("Russian", "Русский");
+		nonLatinTexts.put("Farsi", "فارسی");
+		nonLatinTexts.put("Korean", "한국어");
+		nonLatinTexts.put("Armenian", "Հայերեն");
+		nonLatinTexts.put("Hindi", "हिन्दी");
+		nonLatinTexts.put("Hebrew", "עברית");
+		nonLatinTexts.put("Chinese", "中文");
+		nonLatinTexts.put("Amharic", "አማርኛ");
+		nonLatinTexts.put("Malayalam", "മലയാളം");
+		nonLatinTexts.put("Assyrian Neo-Aramaic", "ܐܬܘܪܝܐ");
+		nonLatinTexts.put("Georgian", "მარგალური");
+
+	}
 
 	public void testString() throws Exception {
-		for (String nonLatinText : nonLatinTexts) {
+		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
+			String language = nonLatinEntry.getKey();
+			String nonLatinText = nonLatinEntry.getValue();
+
 			String s = "{\"key\":\"" + nonLatinText + "\"}";
 			JSONObject obj = (JSONObject) JSONValue.parse(s);
-			String v = (String) obj.get("key"); // result is incorrect
-			assertEquals(v, nonLatinText);
+			String actual = (String) obj.get("key");
+			assertEquals("Parsing String " + language + " text", nonLatinText, actual);
 		}
 	}
 
 	public void testReader() throws Exception {
-		for (String nonLatinText : nonLatinTexts) {
+		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
+			String language = nonLatinEntry.getKey();
+			String nonLatinText = nonLatinEntry.getValue();
+
 			String s = "{\"key\":\"" + nonLatinText + "\"}";
 			StringReader reader = new StringReader(s);
 			JSONObject obj = (JSONObject) JSONValue.parse(reader);
-
-			String v = (String) obj.get("key"); // result is incorrect
-			assertEquals(v, nonLatinText);
+			String actual = (String) obj.get("key");
+			assertEquals("Parsing StringReader " + language + " text", nonLatinText, actual);
 		}
 	}
 
 	public void testInputStream() throws Exception {
-		for (String nonLatinText : nonLatinTexts) {
+		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
+			String language = nonLatinEntry.getKey();
+			String nonLatinText = nonLatinEntry.getValue();
+
 			String s = "{\"key\":\"" + nonLatinText + "\"}";
 			ByteArrayInputStream bis = new ByteArrayInputStream(s.getBytes("utf8"));
 			JSONObject obj = (JSONObject) JSONValue.parse(bis);
-			String v = (String) obj.get("key"); // result is incorrect
-			assertEquals(v, nonLatinText);
+			String actual = (String) obj.get("key");
+			assertEquals("Parsing ByteArrayInputStream " + language + " text", nonLatinText, actual);
 		}
 	}
 
 	public void testBytes() throws Exception {
-		for (String nonLatinText : nonLatinTexts) {
+		for (Map.Entry<String, String> nonLatinEntry : nonLatinTexts.entrySet()) {
+			String language = nonLatinEntry.getKey();
+			String nonLatinText = nonLatinEntry.getValue();
+
 			String s = "{\"key\":\"" + nonLatinText + "\"}";
 			byte[] bs = s.getBytes("utf8");
 			JSONObject obj = (JSONObject) JSONValue.parse(bs);
-			String v = (String) obj.get("key"); // result is incorrect
-			assertEquals(v, nonLatinText);
+			String actual = (String) obj.get("key");
+			assertEquals("Parsing bytes[] " + language + " text", nonLatinText, actual);
 		}
 	}
 }
