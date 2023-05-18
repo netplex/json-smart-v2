0ac0212847509eedfe54c46086b8bec0210a2456
Harikrishna <harikrishna553@gmail.com>
2023-05-18 05:46:37 +0530

    Fix isWritable method (#147)
    * Fix isWritable method
    
    check for the setter property to verify whether the field is writeable or not
    
    * Add test cases
    
    * Add test cases

---
+++ accessors-smart/src/test/java/com/mindev/pojos/AccessorTestPojo.java
@@ -0,0 +1,30 @@
+package com.mindev.pojos;
+
+public class AccessorTestPojo {
+
+	// Field with only setter method
+	private int writeOnlyField;
+	
+	// Field with only getter method
+	private int readOnlyField;
+	
+	// Field with both getter and setter methods
+	private int readAndWriteableField;
+
+	public void setWriteOnlyField(int writeOnlyField) {
+		this.writeOnlyField = writeOnlyField;
+	}
+
+	public int getReadOnlyField() {
+		return readOnlyField;
+	}
+
+	public int getReadAndWriteableField() {
+		return readAndWriteableField;
+	}
+
+	public void setReadAndWriteableField(int readAndWriteableField) {
+		this.readAndWriteableField = readAndWriteableField;
+	}
+
+}
---
+++ accessors-smart/src/test/java/net/minidev/asm/AccessorTest.java
@@ -0,0 +1,106 @@
+package net.minidev.asm;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.Test;
+
+import com.mindev.pojos.AccessorTestPojo;
+
+public class AccessorTest {
+
+	private static class AcceptAllFilter implements FieldFilter {
+
+		@Override
+		public boolean canUse(Field field) {
+			return true;
+		}
+
+		@Override
+		public boolean canUse(Field field, Method method) {
+			return true;
+		}
+
+		@Override
+		public boolean canRead(Field field) {
+			return true;
+		}
+
+		@Override
+		public boolean canWrite(Field field) {
+			return true;
+		}
+
+	}
+
+	private static class AcceptNoneFilter implements FieldFilter {
+
+		@Override
+		public boolean canUse(Field field) {
+			return false;
+		}
+
+		@Override
+		public boolean canUse(Field field, Method method) {
+			return false;
+		}
+
+		@Override
+		public boolean canRead(Field field) {
+			return false;
+		}
+
+		@Override
+		public boolean canWrite(Field field) {
+			return false;
+		}
+
+	}
+
+	@Test
+	public void testWriteOnlyField() throws NoSuchFieldException, SecurityException {
+
+		Field writeOnlyField = AccessorTestPojo.class.getDeclaredField("writeOnlyField");
+		Accessor accessor = new Accessor(AccessorTestPojo.class, writeOnlyField, new AcceptAllFilter());
+
+		assertTrue(accessor.isWritable());
+		assertFalse(accessor.isReadable());
+
+		accessor = new Accessor(AccessorTestPojo.class, writeOnlyField, new AcceptNoneFilter());
+		assertFalse(accessor.isWritable());
+		assertFalse(accessor.isReadable());
+
+	}
+
+	@Test
+	public void testReadOnlyField() throws NoSuchFieldException, SecurityException {
+
+		Field readOnlyField = AccessorTestPojo.class.getDeclaredField("readOnlyField");
+		Accessor accessor = new Accessor(AccessorTestPojo.class, readOnlyField, new AcceptAllFilter());
+
+		assertFalse(accessor.isWritable());
+		assertTrue(accessor.isReadable());
+
+		accessor = new Accessor(AccessorTestPojo.class, readOnlyField, new AcceptNoneFilter());
+		assertFalse(accessor.isWritable());
+		assertFalse(accessor.isReadable());
+	}
+
+	@Test
+	public void testReadAndWriteableField() throws NoSuchFieldException, SecurityException {
+
+		Field readAndWriteableField = AccessorTestPojo.class.getDeclaredField("readAndWriteableField");
+		Accessor accessor = new Accessor(AccessorTestPojo.class, readAndWriteableField, new AcceptAllFilter());
+
+		assertTrue(accessor.isWritable());
+		assertTrue(accessor.isReadable());
+
+		accessor = new Accessor(AccessorTestPojo.class, readAndWriteableField, new AcceptNoneFilter());
+		assertFalse(accessor.isWritable());
+		assertFalse(accessor.isReadable());
+	}
+
+}
