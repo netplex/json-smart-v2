833c5bdcc6cd13f9d72c1102fdc39f96ef0a145a
Uriel Chemouni <uriel.chemouni@gmail.com>
2024-10-08 00:35:42 +0200

    update version and dates. (#220)
    - add links to mvnrepository.com in all poms.
    - update depencency versions.
    - update copyright years.

--- json-smart/src/main/java/net/minidev/json/writer/FakeMapper.java
+++ json-smart/src/main/java/net/minidev/json/writer/FakeMapper.java
@@ -2,7 +2,7 @@ package net.minidev.json.writer;
 
 
 /*
- *    Copyright 2011-2023 JSON-SMART authors
+ *    Copyright 2011-2024 JSON-SMART authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
